6a912efbd5f8cd5c16962097f7dd0262777be471
Tim Graupmann <tgraupmann@gmail.com>
2017-02-19 20:32:07 -0800

    Connecting proxy to UI controls

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.Threading;
@@ -12,11 +13,13 @@ namespace ChromeSpeechProxy
         const string KEY_CHROME_SPEECH_PROXY = "CHROME_SPEECH_PROXY";
         const string KEY_PROXY_PORT = "PROXY_PORT";
 
-        private HttpListener _mHttpListener = new HttpListener();
+        private HttpListener _mHttpListener = null;
 
         private bool _mWaitForExit = true;
 
         private Thread _mThread = null;
+
+        public static List<string> _mWebGLSpeechDetectionPluginResults = new List<string>();
 
         public Form1()
         {
@@ -48,6 +51,8 @@ namespace ChromeSpeechProxy
             btnStop.Enabled = true;
             lblStatus.Enabled = false;
             txtPort.Enabled = false;
+
+            StartProxy();
         }
 
         private void btnStop_Click(object sender, EventArgs e)
@@ -56,6 +61,8 @@ namespace ChromeSpeechProxy
             btnStop.Enabled = false;
             lblStatus.Enabled = true;
             txtPort.Enabled = true;
+
+            StopProxy();
         }
 
         private int GetProxyPort()
@@ -78,12 +85,217 @@ namespace ChromeSpeechProxy
             return result;
         }
 
-        public void SetProxyPort(int port)
+        private void SetProxyPort(int port)
         {
             Microsoft.Win32.RegistryKey key;
             key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(KEY_CHROME_SPEECH_PROXY);
             key.SetValue(KEY_PROXY_PORT, port.ToString());
             key.Close();
+        }
+
+        private void StartProxy()
+        {
+            SetStatus("Starting Proxy...");
+
+            try
+            {
+                _mWaitForExit = true;
+
+                int port;
+                if (int.TryParse(txtPort.Text, out port))
+                {
+                    _mHttpListener = new HttpListener();
+
+                    _mHttpListener.Prefixes.Add(string.Format("http://*:{0}/", port));
+                    _mHttpListener.Start();
+
+                    ThreadStart ts = new ThreadStart(WorkerThread);
+                    _mThread = new Thread(ts);
+                    _mThread.Start();
+
+                    AppendStatus("Proxy Started!");
+                }
+                else
+                {
+                    AppendStatus("Port is Invalid!");
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendStatus("Failed to start listener: {0}", ex);
+            }
+        }
+
+        private void SetStatus(string msg, params object[] args)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { SetStatus(msg, args); });
+                return;
+            }
+
+            lblStatus.Text = "Status:";
+            AppendStatus(Environment.NewLine);
+            AppendStatus(msg, args);
+            AppendStatus(Environment.NewLine);
+        }
+
+        private void DetectedChrome()
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { DetectedChrome(); });
+                return;
+            }
+
+            lblChrome.Text = "Chrome: [Connected]";
+        }
+
+        private void DetectedUnity()
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { DetectedUnity(); });
+                return;
+            }
+
+            lblUnity.Text = "Unity: [Connected]";
+        }
+
+        private void AppendStatus(string msg, params object[] args)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { AppendStatus(msg, args); });
+                return;
+            }
+
+            lblStatus.Text += string.Format(msg, args) + Environment.NewLine;
+        }
+
+        private void WorkerThread()
+        {
+            // keep listening and send a response
+            while (_mWaitForExit)
+            {
+                if (null != _mHttpListener)
+                {
+                    HttpListenerContext context = null;
+                    try
+                    {
+                        context = _mHttpListener.GetContext();
+
+                        string response = "";
+
+                        if (context.Request.Url.LocalPath.EndsWith("/GetResult"))
+                        {
+                            DetectedUnity();
+                            if (_mWebGLSpeechDetectionPluginResults.Count > 0)
+                            {
+                                response = _mWebGLSpeechDetectionPluginResults[0];
+                                _mWebGLSpeechDetectionPluginResults.RemoveAt(0);
+                            }
+                        }
+                        else if (context.Request.Url.LocalPath.EndsWith("/ProxyData"))
+                        {
+                            DetectedChrome();
+                            System.Collections.Specialized.NameValueCollection parameters = HttpUtility.ParseQueryString(context.Request.Url.Query);
+                            string encodedString = parameters["message"];
+                            if (null != encodedString)
+                            {
+                                byte[] data = Convert.FromBase64String(encodedString);
+                                string decodedString = Encoding.UTF8.GetString(data);
+                                if (!string.IsNullOrEmpty(decodedString))
+                                {
+
+                                }
+                            }
+                        }
+                        else if (context.Request.Url.LocalPath.EndsWith("/Abort"))
+                        {
+                            DetectedUnity();
+                            RunJavaScript("WebGLSpeechDetectionPlugin.Abort()");
+                        }
+                        else if (context.Request.Url.LocalPath.EndsWith("/SetLanguage"))
+                        {
+                            DetectedUnity();
+                            System.Collections.Specialized.NameValueCollection parameters = HttpUtility.ParseQueryString(context.Request.Url.Query);
+                            string lang = parameters["lang"];
+                            if (null != lang)
+                            {
+                                RunJavaScript(string.Format("WebGLSpeechDetectionPlugin.SetLanguage(\"{0}\")", lang));
+                            }
+                        }
+                        else
+                        {
+                            DetectedChrome();
+                            try
+                            {
+                                using (System.IO.StreamReader sr = new System.IO.StreamReader("proxy.html"))
+                                {
+                                    response = sr.ReadToEnd();
+                                }
+                            }
+                            catch (Exception)
+                            {
+
+                            }
+                        }
+
+                        byte[] bytes = UTF8Encoding.UTF8.GetBytes(response);
+                        context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+                        context.Response.OutputStream.Flush();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine("WorkerThread: HTTPListener exception={0}", ex);
+                    }
+                    finally
+                    {
+                        try
+                        {
+                            // close the connection
+                            if (null != context)
+                            {
+                                context.Response.Close();
+                            }
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    }
+                }
+                Thread.Sleep(0);
+            }
+
+            AppendStatus("Worker Exited!");
+        }
+
+        private void RunJavaScript(string js)
+        {
+
+        }
+
+        private void StopProxy()
+        {
+            SetStatus("Stopping Proxy...");
+
+            _mWaitForExit = false;
+
+            _mHttpListener.Abort();
+
+            lblChrome.Text = "Chrome: [Not Connected]";
+            lblUnity.Text = "Unity: [Not Connected]";
+
+            AppendStatus("Proxy Stopped!");
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            StopProxy();
         }
     }
 }
